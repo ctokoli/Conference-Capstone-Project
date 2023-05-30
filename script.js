1a525676cf38b4e51b7f10b4e01ed61b2ba7a475
ctokoli <matumasie@gamil.com>
2023-05-30 21:06:59 +0000

    add linters configurations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const humburger = document.querySelector('.humburger');
 const mainMenu = document.querySelector('.main-menu');
 
-humburger.addEventListener('click', () =>{
-    humburger.classList.toggle('active');
-    mainMenu.classList.toggle('active'); 
+humburger.addEventListener('click', () => {
+  humburger.classList.toggle('active');
+  mainMenu.classList.toggle('active');
 });
