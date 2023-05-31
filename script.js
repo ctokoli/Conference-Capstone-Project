bf951e42582d7d7bc33222cb730adde708e666f1
ctokoli <matumasie@gamil.com>
2023-05-31 13:36:46 +0000

    add changes to html

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const humburger = document.querySelector('.humburger');
 const mainMenu = document.querySelector('.main-menu');
+const speakerContent = document.querySelector('.featured-speakers');
 
 humburger.addEventListener('click', () => {
   humburger.classList.toggle('active');
   mainMenu.classList.toggle('active');
 });
 
-fetch.
+fetch('speakers.json').then((response) => response.json()).then((speakers) => {
+    let placeholder = "";
+    speakers.forEach((speaker) => {
+        placeholder += `
+        <div class="speaker">
+        <img src="${speaker.speaker_image}"/>
+        <div class="speaker-text">
+            <h3>${speaker.name}</h3>
+            <div class="red-tex"><p>${speaker.position}</p></div>
+            <p>${speaker.about_speaker}</p>
+        </div>
+    </div>
+        `;
+    });
+
+
+    speakerContent.innerHTML = placeholder;
+ 
+})
